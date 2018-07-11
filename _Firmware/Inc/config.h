efe4132dbd8e9b91055c837bc3f87b1c3644f240
Hasan <hasan@kaist.ac.kr>
2018-07-11 14:37:12 +0900

    progressing

--- _Firmware/Inc/config.h
+++ _Firmware/Inc/config.h
@@ -2,8 +2,8 @@
 #define CONFIG_H
 
 #define DIGIT_CTRL_PORT GPIOA
-#define SEGMENT_ABFG_PORT GPIOB
-#define SEGMENT_CDEP_PORT GPIOA         // P is used an alias of DP
+#define SEG_ABFG_PORT GPIOB
+#define SEG_CDEP_PORT GPIOA         // P is used an alias of DP
 
 #define DIG1_CTRL_PIN GPIO_PIN_3
 #define DIG2_CTRL_PIN GPIO_PIN_5
@@ -15,7 +15,7 @@
 #define SEG_C_PIN GPIO_PIN_8
 #define SEG_D_PIN GPIO_PIN_10
 #define SEG_E_PIN GPIO_PIN_11
-#define SEG_F_PIN GPIO_PIN_14
+#define SEG_F_PIN GPIO_PIN_13
 #define SEG_G_PIN GPIO_PIN_15
 #define SEG_P_PIN GPIO_PIN_9
 
